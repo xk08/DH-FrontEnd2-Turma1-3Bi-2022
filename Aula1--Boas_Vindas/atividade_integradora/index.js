34062d024ec7446fbbd096e2c5f21d8140efd470
XK08 <gronytzki@gmail.com>
2022-05-17 18:51:25 -0300

    Possível resolução para a atividade da aula 1

---
+++ Aula1--Boas_Vindas/atividade_integradora/index.js
@@ -0,0 +1,17 @@
+/* 
+Prof Marcos Martins - Front end 2
+Possível solução para a atividade da Aula 1 (Boas-vindas) 
+*/
+
+let numeros = [1, 2, 4, 8];
+
+let somaTotalNumeros = 0;
+
+console.log(numeros);
+
+for (let i = 0; i < numeros.length; i++) {
+    somaTotalNumeros = somaTotalNumeros + numeros[i];
+    //somaTotalNumeros += numeros[i]; //Outra maneira de somar
+}
+
+console.log("A soma toda dos números foi: " + somaTotalNumeros);
