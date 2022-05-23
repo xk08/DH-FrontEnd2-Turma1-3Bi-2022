5b5ac1333ef8dd880de67c1362065268976034f9
XK08 <gronytzki@gmail.com>
2022-05-23 20:22:12 -0300

    Arquivos base para aula 5

---
+++ Aula5--ModificandoElementosComJs/Aula/scripts/script_aula5.js
@@ -0,0 +1,18 @@
+///@@ 1: Modelos de texto -  Template Strings
+
+
+
+
+///@@ 2: Modificando elementos HTML com o DOM
+
+
+
+
+
+///@@ 3: Modificando os estilos dos elementos selecionados
+
+
+
+
+
+///@@ 4: Modificando as classes dos elementos selecionados
