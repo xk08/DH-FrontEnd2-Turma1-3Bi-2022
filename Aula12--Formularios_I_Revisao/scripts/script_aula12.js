82fdcefc66dbaf0fcad4f9b96fcf83d0c2d1cdc9
XK08 <gronytzki@gmail.com>
2022-06-08 20:05:36 -0300

    Arquivos aula 12, dia 08/06/22

--- Aula12--Formularios_I_Revisao/scripts/script_aula12.js
+++ Aula12--Formularios_I_Revisao/scripts/script_aula12.js
@@ -63,6 +63,86 @@ botaoSalvar.addEventListener("click", function (evento) {
         console.log(radioSelecionado.value); */
 
         /* 2- Fazendo a normalização das informações */
+
+        normalizandoDadosDoUsuario(nomeSobrenome.value);
         
     }
 });
+
+/* 2- Fazendo a normalização das informações */
+function normalizandoDadosDoUsuario(nomeSobrenomeRecebido) {
+
+    //TRIM -> Retirando espaços do inicio e do fim
+    let nomeSemEspacos = nomeSobrenomeRecebido.trim();
+     console.log("Utilizando o trim()");
+    console.log("Com espaços: " + nomeSobrenomeRecebido);
+    console.log("Sem espaços: " + nomeSemEspacos);
+    console.log("\n"); 
+
+    //toLowerCase()
+    let nomeEmMinusculas = nomeSemEspacos.toLowerCase();
+    console.log(nomeEmMinusculas); 
+
+    let nomeAlterado = transformaTextoEmMinusculas(nomeSemEspacos);
+    console.log(nomeAlterado); 
+
+    //SPLIT() -> Quero separar uma informação presente em uma string
+    //Quero separa o nome do sobrenome
+    let nomeSeparado = nomeAlterado.split(" ");
+     console.log(nomeSeparado);
+    console.log(nomeSeparado[0]); 
+
+    //@@ Alterando o valor fixo do HTML
+    //Invoca/chama a função que realiza a alteração
+    alteraDadosNaPagina(nomeSeparado);
+}
+
+function alteraDadosNaPagina(nomeCompletoRecebido) {
+
+    //Capturandos os elementos HTML que serão alterados pelo js
+    let nomeUsuario = document.getElementById("nomeUsuario");
+    let sobrenomeUsuario = document.getElementById("sobrenomeUsuario");
+    let alturaUsuario = document.getElementById("alturaUsuario");
+    let biografiaUsuario = document.getElementById("biografiaUsuario");
+
+    //Alterando o nome e sobrenome
+    nomeUsuario.innerText = nomeCompletoRecebido[0];
+    sobrenomeUsuario.innerText = nomeCompletoRecebido[1];
+
+    //Altera a biografia
+    biografiaUsuario.innerText = biografia.value;
+
+    //Altera a altura fazendo uma conversão
+    alturaUsuario.innerText = altura.value; //Sem alterar
+    alturaUsuario.innerText = modificaAltura(altura.value); //Usando a função que converte
+
+    //Verificando os termos de uso
+
+    //Captura o elemento checkbox
+    let checkBoxTermos = document.getElementById("checkbox");
+
+    //Captura o texto do checkBox
+    let termosUsuario = document.getElementById("termosUsuario");
+
+    //Verifica se o checkbox foi clicado
+    if (checkBoxTermos.checked) {
+        termosUsuario.innerText = "Concorda com os termos: SIM"
+    } else {
+        termosUsuario.innerText = "Concorda com os termos: NÃO"
+    }
+}
+
+//Converte para minisculas
+function transformaTextoEmMinusculas(textoRecebido) { //poderia fazer um para letras Maisculas
+    return textoRecebido.toLowerCase();
+}
+
+//Converte para maiusculas
+function transformaTextoEmMaiusculas(textoRecebido) {
+    return textoRecebido.toUpperCase();
+}
+
+//Altera a string/caracter para a condição desejada
+function modificaAltura(alturaRecebida) {
+    return alturaRecebida.replace(",", ".")
+}
