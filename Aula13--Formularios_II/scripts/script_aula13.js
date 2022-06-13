5c7df453b677cce1c5ec913aa63086e8ccf36c5b
XK08 <gronytzki@gmail.com>
2022-06-13 20:10:45 -0300

    Aula 13, dia 13/06/22

--- Aula13--Formularios_II/scripts/script_aula13.js
+++ Aula13--Formularios_II/scripts/script_aula13.js
@@ -18,11 +18,22 @@ let radioGrupo = document.getElementsByName("radioGrupo");
 //Capturando o botão de salvar
 let botaoSalvar = document.getElementById("salvar");
 
+//Cria variável de controle
+let nomeFormularioOk = false;
+let emailFormularioOk = false;
+
+ //Bloqueado o botão de salvar ao iniciar a página
+ botaoSalvar.setAttribute("disabled", true);
+ botaoSalvar.style.backgroundColor = "#908E8E"
+
+
 //adiciona um evento ao botão de "salvar"
 botaoSalvar.addEventListener("click", function (evento) {
 
     //Valida se o campo obrigatório, está preenchido
-    if (nomeSobrenome.value) {
+    //if (nomeSobrenome.value) { -> Antigo!
+
+    if (nomeFormularioOk && emailFormularioOk) {
         evento.preventDefault();
 
         /* console.log("Exibindo:");
@@ -65,7 +76,7 @@ botaoSalvar.addEventListener("click", function (evento) {
         /* 2- Fazendo a normalização das informações */
 
         normalizandoDadosDoUsuario(nomeSobrenome.value);
-        
+
     }
 });
 
@@ -74,23 +85,23 @@ function normalizandoDadosDoUsuario(nomeSobrenomeRecebido) {
 
     //TRIM -> Retirando espaços do inicio e do fim
     let nomeSemEspacos = nomeSobrenomeRecebido.trim();
-     console.log("Utilizando o trim()");
+    console.log("Utilizando o trim()");
     console.log("Com espaços: " + nomeSobrenomeRecebido);
     console.log("Sem espaços: " + nomeSemEspacos);
-    console.log("\n"); 
+    console.log("\n");
 
     //toLowerCase()
     let nomeEmMinusculas = nomeSemEspacos.toLowerCase();
-    console.log(nomeEmMinusculas); 
+    console.log(nomeEmMinusculas);
 
     let nomeAlterado = transformaTextoEmMinusculas(nomeSemEspacos);
-    console.log(nomeAlterado); 
+    console.log(nomeAlterado);
 
     //SPLIT() -> Quero separar uma informação presente em uma string
     //Quero separa o nome do sobrenome
     let nomeSeparado = nomeAlterado.split(" ");
-     console.log(nomeSeparado);
-    console.log(nomeSeparado[0]); 
+    console.log(nomeSeparado);
+    console.log(nomeSeparado[0]);
 
     //@@ Alterando o valor fixo do HTML
     //Invoca/chama a função que realiza a alteração
@@ -109,7 +120,7 @@ function alteraDadosNaPagina(nomeCompletoRecebido) {
     nomeUsuario.innerText = nomeCompletoRecebido[0];
     sobrenomeUsuario.innerText = nomeCompletoRecebido[1];
 
-    //Altera o email
+    //Altera a biografia
     emailUsuario.innerText = email.value;
 
     //Altera a altura fazendo uma conversão
@@ -146,3 +157,141 @@ function transformaTextoEmMaiusculas(textoRecebido) {
 function modificaAltura(alturaRecebida) {
     return alturaRecebida.replace(",", ".")
 }
+
+/* REALIZANDO ALGUMAS VALIDAÇÕES */
+
+////////Validando o Nome e sobrenome
+nomeSobrenome.addEventListener("click", function () {
+    console.log("Clicou no campo do Nome e sobrenome");
+    //Altera o fundo do campo ao ser clicado
+    nomeSobrenome.style.backgroundColor = "#E2DCDC"
+});
+
+nomeSobrenome.addEventListener("blur", function () {
+    console.log("Saiu do campo do Nome e sobrenome");
+
+    //Pega o elemento Small
+    let nomeSobrenomeValidacao = document.getElementById('nomeSobrenomeValidacao');
+
+    //Altera o fundo do campo ao sair do click 
+    nomeSobrenome.style.backgroundColor = "#FFFFFF"
+
+    //Valida se o campo É VAZIO
+    if (!nomeSobrenome.value) {
+        // troca o fundo
+        nomeSobrenome.style.backgroundColor = "#E6626299"
+
+        //Seta a mensage no small (e configura)
+        nomeSobrenomeValidacao.innerText = "Campo obrigatório"
+        nomeSobrenomeValidacao.style.color = "#D53A3A"
+        nomeSobrenomeValidacao.style.fontWeight = "bold"
+
+        //Bloqueado o botão de salvar
+        botaoSalvar.setAttribute("disabled", true);
+        //Trocando a cor do botão
+        botaoSalvar.style.backgroundColor = "#908E8E"
+
+        //Seta a variável para falso
+        nomeFormularioOk = false;
+
+    } else if (nomeSobrenome.value.length <= 5) { //Tem que ter mais de 5 caracteres
+        // troca o fundo
+        nomeSobrenome.style.backgroundColor = "#E6626299"
+
+        //Seta a mensage no small (e configura)
+        nomeSobrenomeValidacao.innerText = "O campo deve ter mais de 5 letras"
+        nomeSobrenomeValidacao.style.color = "#D53A3A"
+        nomeSobrenomeValidacao.style.fontWeight = "bold"
+
+        //Bloqueado o botão de salvar
+        botaoSalvar.setAttribute("disabled", true);
+        //Trocando a cor do botão
+        botaoSalvar.style.backgroundColor = "#908E8E"
+
+        //Seta a variável para falso
+        nomeFormularioOk = false;
+
+    } else {  //Se ambos estiverem ok
+        //Retira a mensage do small
+        nomeSobrenomeValidacao.innerText = ""
+
+        //Seta o BG do input como Sucesso
+        nomeSobrenome.style.backgroundColor = "#50D48084"
+
+        //Retorna o botão para "habilitado"
+        botaoSalvar.removeAttribute("disabled");
+        botaoSalvar.style.backgroundColor = "#0b5ed7"
+
+        //Seta a variável de controlle do formulário
+        nomeFormularioOk = true;
+    }
+});
+
+////////Validando O E-MAIL
+email.addEventListener("click", function () {
+    console.log("Clicou no campo do E-mail");
+    //Altera o fundo do campo ao ser clicado
+    email.style.backgroundColor = "#E2DCDC"
+});
+
+email.addEventListener("blur", function () {
+    console.log("Saiu do campo do email");
+
+    //Pega o elemento Small
+    let emailValidacao = document.getElementById('emailValidacao');
+
+    //Altera o fundo do campo ao sair do click 
+    email.style.backgroundColor = "#FFFFFF"
+
+    //Valida se o campo É VAZIO
+    if (!email.value) { //Se não foi preenchido
+        // troca o fundo
+        email.style.backgroundColor = "#E6626299"
+
+        //Seta a mensage no small (e configura)
+        emailValidacao.innerText = "Campo obrigatório"
+        emailValidacao.style.color = "#D53A3A"
+        emailValidacao.style.fontWeight = "bold"
+
+        //Bloqueado o botão de salvar
+        botaoSalvar.setAttribute("disabled", true);
+        //Trocando a cor do botão
+        botaoSalvar.style.backgroundColor = "#908E8E"
+
+        //Seta a variável para falso
+        emailFormularioOk = false;
+
+    //Valida de o email está em um formato correto
+    } else if (!email.value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
+        // troca o fundo
+        email.style.backgroundColor = "#E6626299"
+
+        //Seta a mensage no small (e configura)
+        emailValidacao.innerText = "E-mail inválido"
+        emailValidacao.style.color = "#D53A3A"
+        emailValidacao.style.fontWeight = "bold"
+
+        //Bloqueado o botão de salvar
+        botaoSalvar.setAttribute("disabled", true);
+        //Trocando a cor do botão
+        botaoSalvar.style.backgroundColor = "#908E8E"
+
+        //Seta a variável para falso
+        emailFormularioOk = false;
+
+    } else {
+        //Se o campo estiver ok
+        //Retira a mensage do small
+        emailValidacao.innerText = ""
+
+        //Seta o BG do input como Sucesso
+        email.style.backgroundColor = "#50D48084"
+
+        //Retorna o botão para "habilitado"
+        botaoSalvar.removeAttribute("disabled");
+        botaoSalvar.style.backgroundColor = "#0b5ed7"
+
+        //Seta a variável de controlle do formulário
+        emailFormularioOk = true;
+    }
+});
