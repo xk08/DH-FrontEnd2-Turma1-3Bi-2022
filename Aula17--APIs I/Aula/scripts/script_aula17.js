b81a7d28c71bbf06a3376f38dab6ef5cb96a2898
XK08 <gronytzki@gmail.com>
2022-06-21 20:20:41 -0300

    Aula 17, dia 21/06/22, API 1

--- Aula17--APIs I/Aula/scripts/script_aula17.js
+++ Aula17--APIs I/Aula/scripts/script_aula17.js
@@ -6,9 +6,38 @@ botaoPesquisar.addEventListener('click', function (evento) {
     evento.preventDefault();
 
     if (quantidadeFormulario.value != "") {
-        //Executar a busca a API...
-
         
+        //Documentação da API: https://dog.ceo/dog-api/documentation
+
+        /* Usando o fetch para acessar os recursos da API */
+        fetch(`https://dog.ceo/api/breeds/image/random/${quantidadeFormulario.value}`)//Endpoint da API
+            .then(
+                function (resultado) {
+                    return resultado.json() //Faz a conversão de JSON presente no Body para JS
+                }
+            ).then(
+                function (resultado) {
+                    //Tem acesso as informações da requisição nesta chamada do then
+                    //Percorre a lista de imagens
+                    for (let dog of resultado.message) {
+                        console.log(dog);
+
+                        //Cria o elemento imagem
+                        let img = document.createElement("img")
+                        img.src = dog;
+
+                        //Adiciona ao elemento pai
+                        exibeResultado.appendChild(img);
+                    }
+                }
+            ).
+            catch(
+                //Trata possiveis erros na consulta da API
+                function (erro) {
+                    console.log(erro);
+                }
+            );
+
     } else {
         alert("Você deve informar o número de imagens que deseja buscar")
     }
